7495960a6d731562c406eec2633615274fa2dafc
411697643 <411697643@qq.com>
2016-07-15 18:45:29 +0800

    GM:加progressdialog

--- app/src/main/java/com/gm/demoset/HttpMethods.java
+++ app/src/main/java/com/gm/demoset/HttpMethods.java
@@ -1,14 +1,19 @@
 package com.gm.demoset;
 
+import android.content.Context;
+import android.graphics.Color;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
 import rx.schedulers.Schedulers;
 
 /**
@@ -19,14 +24,18 @@ public class HttpMethods {
     public static final String BASE_URL = "https://api.douban.com/v2/movie/";
 
     private static final int DEFAULT_TIMEOUT = 5;
+    private static Context context;
     private final Retrofit retrofit;
     private final MovieService movieService;
+    private final SweetAlertDialog dialog;
 
-    public static HttpMethods getInstance() {
+    public static HttpMethods getInstance(Context context) {
+        HttpMethods.context = context;
         if (ourInstance == null) {
             synchronized (HttpMethods.class) {
                 if (ourInstance == null) {
                     ourInstance = new HttpMethods();
+
                 }
             }
         }
@@ -44,13 +53,34 @@ public class HttpMethods {
                 .build();
 
         movieService = retrofit.create(MovieService.class);
+        dialog = new SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE);
+        dialog.getProgressHelper().setBarColor(Color.parseColor("#55AAFF"));
+        dialog.setTitleText("加载中...");
+        dialog.setCancelable(false);
+
     }
 
-    public void getMoview(Subscriber<HttpResult<List<Subject>>> subscriber, int start, int count) {
-        movieService.getMovie(start, count)
-                .subscribeOn(Schedulers.io())
-                .unsubscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+    public void getMoview(Subscriber<HttpResult<List<Subject>>> subscriber, int start, int count,boolean isShowProgress) {
+        if (isShowProgress) {
+            movieService.getMovie(start, count)
+                    .subscribeOn(Schedulers.io())
+                    .doOnSubscribe(new Action0() {
+                        @Override
+                        public void call() {
+                            dialog.show();
+                        }
+                    })
+                    .subscribeOn(AndroidSchedulers.mainThread())
+                    .unsubscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(subscriber);
+        } else {
+            movieService.getMovie(start, count)
+                    .subscribeOn(Schedulers.io())
+                    .unsubscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(subscriber);
+        }
+
     }
 }
